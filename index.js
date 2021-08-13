83c4195a864c9f35a0b2069932417e39a7430b97
BearmanCodes <idostuff1225@gmail.com>
2021-08-13 10:58:00 -0400

    August 13, 2021
    Added the country avatar thing. and something else but I forgor :skull:

--- index.js
+++ index.js
@@ -7,19 +7,39 @@ app.get('/', (req, res) => res.send('Bot Up'));
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
 
 // Start the discord bot code
+const data = require("./traits.json");
 const Discord = require('discord.js');
-const randomWords = require('random-words');
+const Canvas = require('canvas');
 require('dotenv').config();
 const client = new Discord.Client();
 
 client.once('ready', () => {
-    console.log("I'm always ready you jerk, I bet you are never ready. stupid idiot face.");
+    console.log("Hello, How are you, I am ready, please help, there is too much raining.");
 })
 
-client.on('message', message => {
+client.on('message', async message => {
     const guild = message.member.guild;
-    //guild.members.cache.find
-    
+    if (message.content.startsWith(`${process.env['PREFIX']}avatar`)){
+        const member = message.mentions.members.first();
+        if (member == null){
+            message.channel.send("Please follow the command with @ing the person who's avatar you want to country-fy");
+            return;
+        } else {
+            var memberAvatar = member.user.displayAvatarURL({ format: 'png', size: 128});
+            const canvas = Canvas.createCanvas(128, 128);
+            const context = canvas.getContext('2d');
+            const background = await Canvas.loadImage(memberAvatar);
+            context.drawImage(background, 0, 0, canvas.width, canvas.height);
+            context.globalAlpha = 0.32;
+            var countryNum = Math.floor(Math.random() * 137);
+            var country = await Canvas.loadImage(`C:/Users/Elijah/Desktop/GitHub/Egotistical-discord-bot/Flags/${countryNum}.png`);
+            context.drawImage(country, 0, 0, canvas.width, canvas.height);
+            var attachment = new Discord.MessageAttachment(canvas.toBuffer(), "image.png");
+            message.channel.send(attachment);
+            return;
+        }
+    }
+
     if (message.content.startsWith(`${process.env['PREFIX']}pin`)){
         // I made a get member thing here. Might use this later so I'm gonna use it here but it's useless for pinning.
         /*
@@ -41,32 +61,10 @@ client.on('message', message => {
         message.channel.send("", { files: [fartMp3] });
 
     }
-
-    if (message.content.startsWith(`${process.env['PREFIX']}playfart`)){
-        async function playFart()
-        {
-            if (message.member.voice.channel){
-                var fartNum = Math.floor(Math.random() * 48);
-                var fartMp3 = "https://github.com/BearmanCodes/Egotistical-discord-bot/raw/master/audio/farts/" + fartNum + ".mp3";
-                const connection = await message.member.voice.channel.join();
-                const dispatcher = connection.play(fartMp3);
-
-                dispatcher.on('finish', () => {
-                    dispatcher.destroy();
-                })
-                dispatcher.on('error', () => {
-                    console.error
-                    dispatcher.destroy();
-                });
-                dispatcher.destroy();
-            }
-        }
-        playFart();
-    }
-
-    
 });
 
+
+//Animal Abuser Role bitfield 104320576 , Egotistical bot bitfield 1176612416
 
 client.login(process.env['TOKEN']);
 
